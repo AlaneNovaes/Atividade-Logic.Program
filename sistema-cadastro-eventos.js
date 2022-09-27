a68efb0ef494e53404ace966c40343e2a88f32c1
Alane Novaes <Alanynovaes21@gmail.com>
2022-09-27 20:13:37 -0300

    Create sistema-cadastro-eventos.js
    Adicionado Atividade online 2

---
+++ sistema-cadastro-eventos.js
@@ -0,0 +1,24 @@
+console.log(" Sétima Conferencia de Tecnologia IA ")
+console.log("------------")
+console.log(" Palestrantes ")
+console.log(" Adriano Malagoli e Carlos Augusto Roque ")
+
+console.log("------------")
+
+if (listaParticipantes.length < 100) {
+    console.log(" Permitir cadastro ")
+} else {console.log("Cadastro nao permitido. Vagas esgotadas!")
+}
+
+console.log("-----------")
+
+if (idadeParticipante >= 18) {
+    console.log(" Efetuar cadastro ") 
+} else {console.log(" Cadastro nao permitido para menores de 18 anos! ")
+} 
+                                                                                                                                                                  
+console.log("------------")
+
+let listaParticipantes = ["Jeferson", "Messias ", " Isaias", " Filipe", " Heitor"]
+
+console.log("-------------")
